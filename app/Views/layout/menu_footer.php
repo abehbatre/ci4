2d4c171df25a7b53a73470dc17da7170579c6328
Aditya Ad <abehbatre@gmail.com>
2022-03-26 21:07:10 +0700

    first commit

---
+++ app/Views/layout/menu_footer.php
@@ -0,0 +1,6 @@
+<div class="footer-left">
+    Made with ❤️ <?= date('Y') ?> By <a href="https://github.com/abehbatre" target="_blank">MDVK</a>
+</div>
+<div class="footer-right">
+    Version <?= getenv('APP_VERSION') ?>
+</div>
